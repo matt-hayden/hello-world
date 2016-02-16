bbc6d65c47060358556518421d210d30a7948a52
Matt Hayden <hayden+github@morp.holo.gy>
2016-02-15 17:54:59 -0700

    Initial commit, tab-correct

---
+++ c/Zed/ex19/ex19.h
@@ -0,0 +1,40 @@
+#ifndef _ex19_h
+#define _ex19_h
+
+#include "object.h"
+
+typedef struct Monster {
+	Object proto;
+	int hit_points;
+} Monster;
+
+int Monster_attack(void *self, int damage);
+int Monster_init(void *self);
+
+typedef struct Room {
+	Object proto;
+
+	Monster *bad_guy;
+
+	struct Room *north;
+	struct Room *south;
+	struct Room *east;
+	struct Room *west;
+} Room;
+
+void *Room_move(void *self, Direction direction);
+int Room_attack(void *self, int damage);
+int Room_init(void *self);
+
+
+typedef struct Map {
+	Object proto;
+	Room *start;
+	Room *location;
+} Map;
+
+void *Map_move(void *self, Direction direction);
+int Map_attack(void *self, int damage);
+int Map_init(void *self);
+
+#endif
