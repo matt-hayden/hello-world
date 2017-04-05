096bc36000ab84f0950c4415eec1c4523c864b0e
Matt Hayden (Valenceo LTD) <hayden+git@morp.holo.gy>
2017-04-04 21:10:42 -0600

    Log version added

--- c/factorial/fact.c
+++ c/factorial/fact.c
@@ -2,6 +2,20 @@
 
 
 long double
+odd_fact_log2(unsigned index) {
+  static long double memo[MAX_FACT_INDEX+1] = { 0., 0. };
+  if (index < 2) return 0;
+  if (MAX_FACT_INDEX < index) {
+    return INFINITY;
+  }
+  if (!memo[index]) {
+    memo[index] = (index%2) ? odd_fact_log2(index-2)+log2(index) : odd_fact_log2(index-1);
+  }
+  return memo[index];
+}
+
+
+long double
 odd_fact(unsigned index) {
   static long double memo[MAX_FACT_INDEX+1] = { 1., 1. };
   if (MAX_FACT_INDEX < index) {
@@ -9,6 +23,20 @@ odd_fact(unsigned index) {
   }
   if (!memo[index]) {
     memo[index] = (index%2) ? odd_fact(index-2)*index : odd_fact(index-1);
+  }
+  return memo[index];
+}
+
+
+long double
+fact_log2(unsigned index) {
+  static long double memo[MAX_FACT_INDEX+1] = { 0., 0. };
+  if (index < 2) return 0;
+  if (MAX_FACT_INDEX < index) {
+    return INFINITY;
+  }
+  if (!memo[index]) {
+    memo[index] = odd_fact_log2(index)+fact_log2(index/2)+(index/2);
   }
   return memo[index];
 }
@@ -31,9 +59,8 @@ fact(unsigned index) {
 
 
 long double
-choose(unsigned numerator, unsigned denominator) {
+choose_log2(unsigned numerator, unsigned denominator) {
   if (denominator <= numerator) return
-    fact_over_2en(numerator)/
-    (fact_over_2en(denominator)*fact_over_2en(numerator-denominator));
+    fact_log2(numerator) - fact_log2(denominator) - fact_log2(numerator-denominator);
   return 0.;
 }
